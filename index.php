a23c69fa12898d46aa1b55781dec102cc0921ce4
Martin <mmartinr25@ilb.cat>
2026-04-07 21:45:32 +0200

    afegida de les 3 pagines i taula de multiplicar

--- index.php
+++ index.php
@@ -1,11 +1,38 @@
+<?php
+$numero = 7;
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ca">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Taula de multiplicar</title>
 </head>
 <body>
-    
+
+<h1>Taula del <?php echo $numero; ?></h1>
+
+<?php
+if ($numero < 1 || $numero > 12) {
+    echo "<p>Error: número incorrecte</p>";
+} else {
+    echo "<table border='1'>";
+
+    for ($i = 1; $i <= 10; $i++) {
+
+        if ($i % 2 == 0) {
+            echo "<tr style='background-color: lightgray'>";
+        } else {
+            echo "<tr>";
+        }
+
+        echo "<td>" . $numero . " x " . $i . "</td>";
+        echo "<td>" . ($numero * $i) . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+?>
+
 </body>
 </html>
